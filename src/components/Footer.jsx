5297e98cae31f4bd08aed086fe2d90ee5a542a5a
Joe Buckle <joe@white-fire.co.uk>
2018-09-18 14:51:44 +0100

    Hide footer on mobiles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,80 +28,82 @@ class Footer extends Component {
     render() {
         const { classes } = this.props;
         return (
-            <Grid container className={classes.root} spacing={16}>
-                <Grid item xs={12}>
-                    <Grid container justify="center" >
-                        <Grid>
-                            <Button
-                                component={Link}
-                                to='/'
-                                variant="contained"
-                                color="default"
-                                type='button'
-                                size="small"
-                                className={classes.buttonMargin}
-                            >
-                                Home
-                            </Button>
-                        </Grid>
-                        <Grid>
-                            <Button
-                                component={Link}
-                                to='/about'
-                                variant="contained"
-                                color="default"
-                                type='button'
-                                size="small"
-                                className={classes.buttonMargin}
-                            >
-                                About
-                            </Button>
-                        </Grid>
-                        <Grid>
-                            <Button
-                                component={Link}
-                                to='/manage-sections'
-                                variant="contained"
-                                color="default"
-                                type='button'
-                                size="small"
-                                className={classes.buttonMargin}
-                            >
-                                Manage sections
-                            </Button>
-                        </Grid>
-                        <Grid>
-                            <Button
-                                component={Link}
-                                to='/settings'
-                                variant="contained"
-                                color="default"
-                                type='button'
-                                size="small"
-                                className={classes.buttonMargin}
-                            >
-                                Settings
-                            </Button>
-                        </Grid>
-                        <Hidden smDown>
+            <Hidden smDown>
+                <Grid container className={classes.root} spacing={16}>
+                    <Grid item xs={12}>
+                        <Grid container justify="center" >
                             <Grid>
                                 <Button
                                     component={Link}
-                                    to='/breakout'
+                                    to='/'
                                     variant="contained"
-                                    color="secondary"
+                                    color="default"
                                     type='button'
                                     size="small"
                                     className={classes.buttonMargin}
-
                                 >
-                                    Play Breakout!
+                                    Home
                                 </Button>
                             </Grid>
-                        </Hidden>
+                            <Grid>
+                                <Button
+                                    component={Link}
+                                    to='/about'
+                                    variant="contained"
+                                    color="default"
+                                    type='button'
+                                    size="small"
+                                    className={classes.buttonMargin}
+                                >
+                                    About
+                                </Button>
+                            </Grid>
+                            <Grid>
+                                <Button
+                                    component={Link}
+                                    to='/manage-sections'
+                                    variant="contained"
+                                    color="default"
+                                    type='button'
+                                    size="small"
+                                    className={classes.buttonMargin}
+                                >
+                                    Manage sections
+                                </Button>
+                            </Grid>
+                            <Grid>
+                                <Button
+                                    component={Link}
+                                    to='/settings'
+                                    variant="contained"
+                                    color="default"
+                                    type='button'
+                                    size="small"
+                                    className={classes.buttonMargin}
+                                >
+                                    Settings
+                                </Button>
+                            </Grid>
+                            <Hidden smDown>
+                                <Grid>
+                                    <Button
+                                        component={Link}
+                                        to='/breakout'
+                                        variant="contained"
+                                        color="secondary"
+                                        type='button'
+                                        size="small"
+                                        className={classes.buttonMargin}
+
+                                    >
+                                        Play Breakout!
+                                    </Button>
+                                </Grid>
+                            </Hidden>
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
+            </Hidden>
     );
     }
 
